4db4c0651b929c8c21b381f1de7407fec7cc5163
Ahmed Tawfik <teffa.ahmed.1@gmail.com>
2026-09-03 14:37:33 +0300

    Add exceptions to __all__ for improved error handling in core module

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,3 +1,9 @@
+from src.core.exceptions import OriaAgentException, SpeechToTextError, TextToSpeechError
 from src.core.prompts import MEMORY_ANALYSIS_PROMPT
 
-__all__ = ["MEMORY_ANALYSIS_PROMPT"]
+__all__ = [
+    "MEMORY_ANALYSIS_PROMPT",
+    "OriaAgentException",
+    "SpeechToTextError",
+    "TextToSpeechError",
+]
