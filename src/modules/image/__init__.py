01f77a7052eb3856dacb190052f373544334b099
Ahmed Tawfik <teffa.ahmed.1@gmail.com>
2026-09-05 15:01:06 +0300

    Add TTIProvider and TTIFactory to image module exports

--- src/modules/image/__init__.py
+++ src/modules/image/__init__.py
@@ -3,8 +3,11 @@ from src.modules.image.image_to_text import ImageToText, get_image_to_text
 from src.modules.image.text_to_image import (
     EnhancedPrompt,
     ScenarioPrompt,
+    TTIFactory,
+    TTIProvider,
     TextToImage,
     get_text_to_image,
+    get_tti_provider,
 )
 
 __all__ = [
@@ -16,4 +19,8 @@ __all__ = [
     "get_text_to_image",
     "ScenarioPrompt",
     "EnhancedPrompt",
+    "TTIProvider",
+    "TTIFactory",
+    "get_tti_provider",
 ]
+
